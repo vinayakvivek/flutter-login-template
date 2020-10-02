079d44d6ca485c3cc9f45c5b803109df23583e9d
Vinayak <vinayakdeepu@gmail.com>
2020-10-02 21:18:51 +0530

    Add user model

---
+++ packages/authentication_repository/lib/src/models/user.dart
@@ -0,0 +1,28 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class User extends Equatable {
+
+  const User({
+    @required this.email,
+    @required this.id,
+    @required this.name,
+    @required this.photo,
+  }) : assert(email != null),
+       assert(id != null);
+
+  final String email;
+
+  final String id;
+
+  final String name;
+
+  /// Profile pic url
+  final String photo;
+
+  static const empty = User(email: '', id: '', name: null, photo: null);
+
+  @override
+  List<Object> get props => [email, id, name, photo];
+}
