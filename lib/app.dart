12bf3f680c7049747d88131a8964c6f37c0e86bf
Vinayak <vinayakdeepu@gmail.com>
2020-10-03 11:58:40 +0530

    Add App widget

---
+++ lib/app.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({
+    Key key,
+    @required this.authenticationRepository,
+  })  : assert(authenticationRepository != null),
+        super(key: key);
+
+  final AuthenticationRepository authenticationRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => null,
+        child: AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  @override
+  _AppViewState createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Text('Yo'),
+      ),
+    );
+  }
+}
---
+++ lib/components/splash/view/splash_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  static Route route() {
+    return MaterialPageRoute<void>(builder: (_) => SplashPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/auth_logo.png',
+          key: const Key('splash_image'),
+          width: 150,
+        ),
+      ),
+    );
+  }
+}
